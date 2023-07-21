86f58bb8216cff93d4a28617adaf51382bd103b3
lsm0506 <tkdals2317@gmail.com>
2023-07-21 19:26:07 +0900

    6강. 코틀린에서 반복문을 다루는 방법

---
+++ src/main/kotlin/com/sangmin/lec6/Lec06Main.java
@@ -0,0 +1,29 @@
+package com.sangmin.lec6;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lec06Main {
+
+    public void printForEach() {
+        List<Long> numbers = Arrays.asList(1L, 2L, 3L);
+        for (Long number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    public void printFor() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+    public void printWhile() {
+        int i = 1;
+        while (i <= 3) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/main/kotlin/com/sangmin/lec6/main.kt
@@ -0,0 +1,46 @@
+package com.sangmin.lec6
+
+fun main() {
+}
+
+// 향상된 for문
+fun printForEach() {
+    val numbers = listOf(1L, 2L, 3L) // 컬렉션을 만드는 방법
+    for (number in numbers) { // : 대신 in을 사용
+        println(number)
+    }
+}
+
+// 전통적인 for문
+fun printFor() {
+    for(i in 1..3) {
+        println(i)
+    }
+}
+
+// 전통적인 for문(역순)
+fun printForReverse() {
+    for (i in 3 downTo 1) {
+        println(i)
+    }
+}
+
+// 전통적인 for문(2개씩 증가)
+fun printForStep2() {
+    for(i in 1..5 step 2) {
+        println(i)
+    }
+}
+
+// while문
+fun printWhile() {
+    var i = 1;
+    while (i <= 3) {
+        println(i)
+        i++
+    }
+}
+
+
+
+
