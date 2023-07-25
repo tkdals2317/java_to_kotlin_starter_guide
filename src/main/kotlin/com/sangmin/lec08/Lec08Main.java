67b2b12b15194e33e520b88b5b82fcd4b4019cc9
lsm0506 <tkdals2317@gmail.com>
2023-07-25 21:12:57 +0900

    Lec 08. 코틀린에서 함수를 다루는 방법 - default parameter, named argument, 가변인자

--- src/main/kotlin/com/sangmin/lec08/Lec08Main.java
+++ src/main/kotlin/com/sangmin/lec08/Lec08Main.java
@@ -2,11 +2,48 @@ package com.sangmin.lec08;
 
 public class Lec08Main {
 
-    public int max(int a, int b){
+    /* 1. 함수 선언 문법 */
+    public int max(int a, int b) {
         if (a > b) {
             return a;
         }
         return b;
+    }
+
+    /* 2. default parameter */
+    public void repeat(String str, int num, boolean useNewLine) {
+        for (int i = 0; i <= num; i++) {
+            if (useNewLine) {
+                System.out.println(str);
+            } else {
+                System.out.print(str);
+            }
+        }
+    }
+
+    // 많은 코드에서 useNewline을 true로 사용한다면? Java의 오버로딩을 활용
+    public void repeat(String str, int num) {
+        repeat(str, num, true);
+    }
+
+    // 많은 코드에서 출력을 3회씩 하고 있다면? 다시 한번 오버로딩! 총 3개의 함수가 존재
+    public void repeat(String str) {
+        repeat(str, 3, true);
+    }
+
+
+    /* 4. 같은 타입의 여러 파라미터 받기(가변인자) */
+    public static void printAll(String... strings) {
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"A", "B", "C"};
+        // 배열을 직접 넣거나, comma를 이용해 여러 파라미터를 넣는다.
+        printAll(array);
+        printAll("A", "B", "C");
     }
 
 }
--- src/main/kotlin/com/sangmin/lec08/main.kt
+++ src/main/kotlin/com/sangmin/lec08/main.kt
@@ -3,7 +3,7 @@ package com.sangmin.lec08
 fun main() {
 }
 
-// 함수 선언 문법
+/* 1. 함수 선언 문법 */
 public fun max(a: Int, b: Int): Int {
     if (a > b) {
         return a
@@ -34,3 +34,42 @@ fun maxV4(a: Int, b: Int): Int = if (a > b) a else b
 // = 을 사용하는 경우 반환 타입 생략 가능
 fun maxV5(a: Int, b: Int) = if (a > b) a else b
 
+
+/* 2. default parameter */
+// 코틀린에서는 디폴트 파라미터를 설정할 수 있다.
+// 밖에서 파라미터를 넣어주지 않으면 기본값을 사용하자
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
+
+// repeat을 호출할 때 num은 3을 그대로 쓰고, useNewLine은 false를 쓰고 싶다면?
+/* 3. named argument */
+fun callRepeat() {
+    // 매개변수 이름을 통해 직접 지정, 지정되지 않은 매개변수는 기본값을 사용
+    // => builder를 직접 만들지 않고 builder의 장점을 가지게 된다.
+    repeat("Hello World", useNewLine = false)
+}
+
+/* 4. 같은 타입의 여러 파라미터 받기(가변인자) */
+fun printAll(vararg strings: String) { // ...을 타입 뒤에 쓰는 대신 제일 앞에 vararg를 적어주어야한다.
+    for (str in strings) {
+        println(str)
+    }
+}
+
+fun callPrintAll() {
+    val array = arrayOf("A", "B", "C")
+    // 배열을 바로 넣는 대신 스프레드 연산자(*)를 붙여주어야 한다.
+    printAll(*array)
+    printAll("A", "B", "C")
+}
